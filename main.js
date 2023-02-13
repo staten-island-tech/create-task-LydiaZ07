d228fa80d5c52ea1429a5cec6317257ffffb161e
LydiaZ07 <zhulydia07@gmail.com>
2023-02-13 13:14:44 -0500

    2

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 console.log("HI");
 
  function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  min = Math.ceil(min);//rounds up to min, inclusive
+  max = Math.floor(max);//rounds to lower number, exclusive 
+  return Math.floor(Math.random() * (max - min) + min); 
 };
-console.log(getRandomInt(1,101));
+console.log(getRandomInt(1,101));//random number between 1 and 100, inclusive 
+
+//https://cataas.com/cat  cat img gen api
+//https://api.goprogram.ai/inspiration motivational/inspirational quote gen api 
