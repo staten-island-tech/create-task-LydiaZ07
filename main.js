e5bec97863e62989c87c0627e2b89c9f47f1984f
LydiaZ07 <113201279+LydiaZ07@users.noreply.github.com>
2023-02-13 13:08:33 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("HI");
+
+ function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+};
+console.log(getRandomInt(1,101));
