86d26c99028036586b6f1d46d33322446c793682
LydiaZ07 <zhulydia07@gmail.com>
2023-02-16 13:14:52 -0500

    l

--- js/main.js
+++ js/main.js
@@ -12,11 +12,41 @@ console.log("HI");
 //https://cataas.com/cat  cat img gen api
 //https://api.goprogram.ai/inspiration motivational/inspirational quote gen api 
 
-// DOMSelectors.btn.addEventListener("click", function(e) {
-//   e.preventDefault();
-//     console.log(getRandomInt(1,101));
+DOMSelectors.btn.addEventListener("click", function(e) {
+  e.preventDefault();
+    console.log(getRandomInt(1,101));
 
-// }); 
+}); 
+
+function displayQuote(data){
+  DOMSelectors.display.innerHTML = "";
+  DOMSelectors.display.insertAdjacentHTML(
+      "afterbegin", `<div class="output">
+      <p>#${data.quote}</p>
+      <p>"${data.author}"</p></div>`
+  );
+}    
+
+async function getData(num){
+    
+  try {
+      const response = await fetch(`https://api.goprogram.ai/inspiration${num}`);
+      if(response.status < 200 || response.status >299){
+          console.log(response.status);
+          throw new error(response);
+      } else{
+          const data = await response.json();
+
+      console.log(data);
+     displayQuote(data); 
+  };
+  } catch (error) {
+      console.log(error);
+      alert("ERROR! Please try again.");
+     
+  }};
+
+
 
 // function generateNum(){
 //   DOMSelectors.display.insertAdjacentElement(`beforeend`, `<div>You have generated the number:</div>`)
