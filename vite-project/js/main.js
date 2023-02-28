fc201db159f27df555640c814925a70d64e4519e
LydiaZ07 <zhulydia07@gmail.com>
2023-02-28 13:07:41 -0500

    c

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -14,57 +14,29 @@ console.log("HI");
 
 DOMSelectors.btn.addEventListener("click", function(e) {
   e.preventDefault();
-    console.log(getRandomInt(1,225));
-    const hello = getRandomInt(1,225).innerHTML;
+    console.log(getRandomInt(-20,20));
+    const hello = getRandomInt(-20,20).innerHTML;
 console.log(hello);
-DOMSelectors.numb.innerHTML = getRandomInt(1,225);
-const dis = DOMSelectors.display.value
+DOMSelectors.numb.innerHTML = getRandomInt(-10,10);
+const dis = DOMSelectors.display.value;
     console.log(dis);
     getData(dis);
     DOMSelectors.display.value = "";
 }); 
 
 
-function displayQuote(data){
-  DOMSelectors.display.innerHTML = "";
-  DOMSelectors.display.insertAdjacentHTML(
-      "afterbegin", `<div class="output">
-      <p>"${data.slip.advice}"</p></div>`
-  );
-};
-
-async function getData(){
-  
-  try {
-      const response = await fetch(`https://api.adviceslip.com/advice/${getRandomInt(1,225)}`);
-      if(response.status < 200 || response.status >299){
-          console.log(response.status);
-          throw new error(response);
-      } else{
-          const data = await response.json();
-
-      console.log(data);
-     displayQuote(data); 
-  };
-  } catch (error) {
-      console.log(error);
-      alert("ERROR! Please try again.");
-     
-  }};
-
- 
 // function displayQuote(data){
 //   DOMSelectors.display.innerHTML = "";
 //   DOMSelectors.display.insertAdjacentHTML(
 //       "afterbegin", `<div class="output">
-//       <img ${data.url}></div>`
+//       <p>"${data.slip.advice}"</p></div>`
 //   );
 // };
 
 // async function getData(){
   
 //   try {
-//       const response = await fetch(`https://api.thecatapi.com/v1/images/search`);
+//       const response = await fetch(`https://api.adviceslip.com/advice/${getRandomInt(1,225)}`);
 //       if(response.status < 200 || response.status >299){
 //           console.log(response.status);
 //           throw new error(response);
@@ -79,6 +51,34 @@ async function getData(){
 //       alert("ERROR! Please try again.");
      
 //   }};
+
+ 
+function displayQuote(data){
+  DOMSelectors.display.innerHTML = "";
+  DOMSelectors.display.insertAdjacentHTML(
+      "afterbegin", `<div class="output">
+      <img src="${data.url}"></div>`
+  );
+};
+
+async function getData(){
+  
+  try {
+      const response = await fetch(`https://api.thecatapi.com/v1/images/search`);
+      if(response.status < 200 || response.status >299){
+          console.log(response.status);
+          throw new error(response);
+      } else{
+          const data = await response.json();
+
+      console.log(data);
+     displayQuote(data); 
+  };
+  } catch (error) {
+      console.log(error);
+      alert("ERROR! Please try again.");
+     
+  }};
 
 
 
